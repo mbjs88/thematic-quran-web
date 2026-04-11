53f2ef8f2b0317e02fd3ad8bd1d0fe59e2ac6d37
Thematic Quran <thematicquran.contact@gmail.com>
2026-04-11 20:33:21 +1000

    feat: add reading_session scope to login authorization request

--- functions/auth/login.js
+++ functions/auth/login.js
@@ -9,7 +9,7 @@ export async function onRequest(context) {
         const REDIRECT_URI = "https://thematicquran.com/auth/callback";
         const { url, state, nonce, codeVerifier } = await buildAuthorizationUrl(env, {
             redirectUri: REDIRECT_URI,
-            scopes: "offline_access user collection"
+            scopes: "offline_access user collection reading_session"
         });
         
         const headers = new Headers();
